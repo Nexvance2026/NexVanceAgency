148e16354e6734eb4b837a0738e769fe082df11e
Nexvance2026 <haseebsoomro2k19@gmail.com>
2026-06-22 00:31:55 +0500

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { App } from './App.tsx';
-import './index.css' // Hamari styling file jo hum agle step mein banayenge
+import App from './App.tsx' // No brackets here!
+import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
