7379fdd9f686ac4f7a048e868f7db2e94c6e495d
Nexvance2026 <haseebsoomro2k19@gmail.com>
2026-06-21 18:41:24 +0500

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
+import { App } from './App.tsx';
 import './index.css' // Hamari styling file jo hum agle step mein banayenge
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
